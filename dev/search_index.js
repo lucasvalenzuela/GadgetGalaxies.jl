08f3cfa6dd885c76919d9eaf21cfb9d9463b4722
Documenter.jl <documenter@juliadocs.github.io>
2021-05-01 13:29:12 +0000

    build based on 2e1830a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GadgetGalaxies","category":"page"},{"location":"#GadgetGalaxies","page":"Home","title":"GadgetGalaxies","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GadgetGalaxies.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GadgetGalaxies]","category":"page"}]
+}
